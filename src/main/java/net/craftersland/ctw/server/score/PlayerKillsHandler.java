b7e80223d756d8f44dabcdac63302fc40c926115
Ricardo Araque <ricardoaraque@outlook.com>
2025-02-09 08:16:39 -0600

    Arreglado /shop como espectador

--- src/main/java/net/craftersland/ctw/server/score/PlayerKillsHandler.java
+++ src/main/java/net/craftersland/ctw/server/score/PlayerKillsHandler.java
@@ -4,6 +4,7 @@ import net.craftersland.ctw.server.CTW;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Comparator;
 import java.util.HashMap;
@@ -16,18 +17,27 @@ public class PlayerKillsHandler {
     private final Map<Player, Integer> totalKills;
     private final Map<Player, Integer> bowKills;
     private final Map<Player, Integer> meleeKills;
+
+    // Match stats
     private final HashMap<String, Integer> kills;
+    private final HashMap<String, Integer> meleeKillsMatch;
+    private final HashMap<String, Integer> bowKillsMatch;
+
 
     public PlayerKillsHandler(final CTW ctw) {
-        this.totalKills = new HashMap<Player, Integer>();
-        this.bowKills = new HashMap<Player, Integer>();
-        this.meleeKills = new HashMap<Player, Integer>();
-        this.kills = new HashMap<String, Integer>();
+        this.totalKills = new HashMap<>();
+        this.bowKills = new HashMap<>();
+        this.meleeKills = new HashMap<>();
+
+        // Match stats
+        this.kills = new HashMap<>();
+        this.meleeKillsMatch = new HashMap<>();
+        this.bowKillsMatch = new HashMap<>();
         this.ctw = ctw;
     }
 
     public void saveAllData() {
-        final Map<Player, Integer> data = new HashMap<Player, Integer>(this.totalKills);
+        final Map<Player, Integer> data = new HashMap<>(this.totalKills);
         if (!data.isEmpty()) {
             for (final Player p : data.keySet()) {
                 if (p != null && p.isOnline()) {
@@ -70,22 +80,7 @@ public class PlayerKillsHandler {
     }
 
     public void resetKills() {
-
         kills.clear();
-
-    }
-
-    public void addKill(String name) {
-
-        if (!kills.containsKey(name)) {
-
-            kills.put(name, 1);
-
-        } else {
-
-            kills.put(name, kills.get(name) + 1);
-
-        }
     }
 
     public void saveKillsToDatabasse(final Player p) {
@@ -111,20 +106,17 @@ public class PlayerKillsHandler {
     }
 
     public void addBowKill(final Player p) {
-        final int kills = this.bowKills.get(p);
-        final int tkills = this.totalKills.get(p);
+        final int bowKills = this.bowKills.get(p);
+        final int totalKills = this.totalKills.get(p);
         try {
-            this.bowKills.put(p, kills + 1);
-            this.totalKills.put(p, tkills + 1);
+            this.bowKills.put(p, bowKills + 1);
+            this.totalKills.put(p, totalKills + 1);
+            this.bowKillsMatch.put(p.getName(), this.bowKillsMatch.get(p.getName()) + 1);
+            this.kills.put(p.getName(), this.kills.get(p.getName()) + 1);
             this.addTeamKill(p);
         } catch (Exception e) {
             if (p != null) {
-                Bukkit.getScheduler().runTask(this.ctw, new Runnable() {
-                    @Override
-                    public void run() {
-                        p.kickPlayer(PlayerKillsHandler.this.ctw.getLanguageHandler().getMessage("KickMessages.Error"));
-                    }
-                });
+                Bukkit.getScheduler().runTask(this.ctw, () -> p.kickPlayer(PlayerKillsHandler.this.ctw.getLanguageHandler().getMessage("KickMessages.Error")));
             }
         }
     }
@@ -135,6 +127,7 @@ public class PlayerKillsHandler {
             kills = this.meleeKills.get(p);
         }
         this.meleeKills.put(p, kills + 1);
+        this.addMeleeKillMatch(p);
         int tkills = 0;
         if (this.totalKills.containsKey(p)) {
             tkills = this.totalKills.get(p);
@@ -144,18 +137,15 @@ public class PlayerKillsHandler {
     }
 
     public int getTotalKills(final Player p) {
-        final int kills = this.totalKills.get(p);
-        return kills;
+        return this.totalKills.get(p);
     }
 
     public int getBowKills(final Player p) {
-        final int kills = this.bowKills.get(p);
-        return kills;
+        return this.bowKills.get(p);
     }
 
     public int getMeleeKills(final Player p) {
-        final int kills = this.meleeKills.get(p);
-        return kills;
+        return this.meleeKills.get(p);
     }
 
     private void addTeamKill(final Player p) {
@@ -164,5 +154,56 @@ public class PlayerKillsHandler {
         } else if (this.ctw.getTeamHandler().isBlueTeam(p)) {
             this.ctw.getTeamKillsHandler().addBlueKill();
         }
+    }
+
+    public void addKillMatch(final @NotNull Player p) {
+        final String name = p.getName();
+        if (!this.kills.containsKey(name)) {
+            this.kills.put(name, 1);
+        } else {
+            this.kills.put(name, this.kills.get(name) + 1);
+        }
+    }
+
+    public void addBowKillMatch(final @NotNull Player p) {
+        final String name = p.getName();
+        if (!this.bowKillsMatch.containsKey(name)) {
+            this.bowKillsMatch.put(name, 1);
+        } else {
+            this.bowKillsMatch.put(name, this.bowKillsMatch.get(name) + 1);
+        }
+    }
+
+    public void addMeleeKillMatch(final @NotNull Player p) {
+        final String name = p.getName();
+        if (!this.meleeKillsMatch.containsKey(name)) {
+            this.meleeKillsMatch.put(name, 1);
+        } else {
+            this.meleeKillsMatch.put(name, this.meleeKillsMatch.get(name) + 1);
+        }
+    }
+
+    public int getKillsMatch(final @NotNull Player p) {
+        final String name = p.getName();
+        if (this.kills.containsKey(name)) {
+            return this.kills.get(name);
+        }
+        return 0;
+    }
+
+    public int getBowKillsMatch(final @NotNull Player p) {
+        final String name = p.getName();
+        if (this.bowKillsMatch.containsKey(name)) {
+            return this.bowKillsMatch.get(name);
+        }
+        return 0;
+    }
+
+    public int getMeleeKillsMatch(final @NotNull Player p) {
+        final String name = p.getName();
+        if (this.meleeKillsMatch.containsKey(name)) {
+            return this.meleeKillsMatch.get(name);
+        }
+        return 0;
     }
 }
