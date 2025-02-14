69e362c028c2e4b1cdaf4f8da75db136a6a72f76
Ricardo Araque <ricardoaraque@outlook.com>
2025-02-14 08:41:16 -0600

    Optimizado y fix instant respawn

--- src/main/java/net/craftersland/ctw/server/events/Death.java
+++ src/main/java/net/craftersland/ctw/server/events/Death.java
@@ -87,16 +87,6 @@ public class Death implements Listener {
     }
 
     @EventHandler
-    public void onDeath(final @NotNull PlayerDeathEvent e) {
-        final Player killer = e.getEntity().getKiller();
-
-        if (killer == null) {
-            return;
-        }
-        killer.playSound(killer.getLocation(), Sound.LEVEL_UP, 3.0f, 3.0f);
-    }
-
-    @EventHandler
     public void onPlayerDeath(final @NotNull PlayerDeathEvent event) {
         event.setDeathMessage("");
 
@@ -112,13 +102,13 @@ public class Death implements Listener {
         this.ctw.getWoolHandler().listPlayersblue().remove(player);
         this.ctw.getWoolHandler().listPlayerscyan().remove(player);
 
-        Death.this.ctw.getTakenWools().checkForLostWool(event.getEntity(), event.getDrops());
+        this.ctw.getTakenWools().checkForLostWool(event.getEntity(), event.getDrops());
         event.getDrops().clear();
 
         Bukkit.getScheduler().runTaskAsynchronously(this.ctw, () -> {
 
             Player p = event.getEntity();
-            Death.this.ctw.getKillStreakHandler().resetData(p);
+            this.ctw.getKillStreakHandler().resetData(p);
 
             if (p.getLastDamageCause() == null) {
 
@@ -129,21 +119,24 @@ public class Death implements Listener {
             if (!(p.getLastDamageCause().getEntity() instanceof Player)) return;
             if (p.getKiller() == null) return;
 
+            Player killer = p.getKiller();
+
+            killer.playSound(killer.getLocation(), Sound.LEVEL_UP, 3.0f, 3.0f);
+
             if (p.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
 
-                Player killer = p.getKiller();
-                String rawMsg1 = Death.this.ctw.getLanguageHandler().getMessage("ChatMessages.MeleeDeathBroadcast").replace("%WeaponLogo%", Death.this.getWeaponLogo(killer));
-                addPoints(p, killer, rawMsg1);
-                Death.this.ctw.getPlayerKillsHandler().addMeleeKill(killer);
-                Death.this.ctw.getMeleeAchievementHandler().checkForAchievements(killer);
-                Death.this.ctw.getOverpoweredAchievementHandler().checkForAchievements(p);
-                Death.this.ctw.getKillStreakHandler().addStreakKill(killer);
+                String rawMsg1 = this.ctw.getLanguageHandler().getMessage("ChatMessages.MeleeDeathBroadcast").replace("%WeaponLogo%", this.getWeaponLogo(killer));
 
+                this.ctw.getPlayerKillsHandler().addMeleeKill(killer);
+                this.ctw.getMeleeAchievementHandler().checkForAchievements(killer);
+                this.ctw.getOverpoweredAchievementHandler().checkForAchievements(p);
+                this.ctw.getKillStreakHandler().addStreakKill(killer);
+                this.ctw.getPlayerKillsHandler().addMeleeKillMatch(killer);
+
+                addPoints(p, killer, rawMsg1);
                 addRegen(killer);
 
             } else if (p.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.PROJECTILE) {
-                Player killer = p.getKiller();
-                addRegen(killer);
                 int distance;
 
                 try {
@@ -151,83 +144,82 @@ public class Death implements Listener {
                     distance = pj.getProjectile(p.getUniqueId());
                 } catch (NullPointerException e) {
                     distance = (int) p.getLocation().distance(killer.getLocation());
+                    e.printStackTrace();
                 }
 
-                final String rawMsg4 = Death.this.ctw.getLanguageHandler().getMessage("ChatMessages.BowDeathBroadcast").replace("%WeaponLogo%", Death.this.getWeaponLogo(killer));
-                final String rawMsg5 = rawMsg4.replace("%KilledPlayer%", Death.this.ctw.getMessageUtils().getTeamColor(p));
-                final String rawMsg6 = rawMsg5.replace("%Killer%", Death.this.ctw.getMessageUtils().getTeamColor(killer));
-                final String rawMsg7 = rawMsg6.replace("%distance%", String.valueOf(distance));
-                addPoints2(p, killer, rawMsg7);
-                Death.this.ctw.getPlayerKillsHandler().addBowKill(killer);
-                Death.this.ctw.getPlayerBowDistanceKillHandler().setDistanceKill(killer, distance);
-                Death.this.ctw.getShooterAchievementHandler().checkForAchievements(killer);
-                Death.this.ctw.getOverpoweredAchievementHandler().checkForAchievements(killer);
-                Death.this.ctw.getDistanceAchievementHandler().checkForAchievements(killer);
-                Death.this.ctw.getKillStreakHandler().addStreakKill(killer);
+                final String bowDeathBroadcast = this.ctw.getLanguageHandler().getMessage("ChatMessages.BowDeathBroadcast")
+                        .replace("%WeaponLogo%", this.getWeaponLogo(killer))
+                        .replace("%KilledPlayer%", this.ctw.getMessageUtils().getTeamColor(p))
+                        .replace("%Killer%", this.ctw.getMessageUtils().getTeamColor(killer))
+                        .replace("%distance%", String.valueOf(distance));
+                
+                this.ctw.getPlayerKillsHandler().addBowKill(killer);
+                this.ctw.getPlayerBowDistanceKillHandler().setDistanceKill(killer, Integer.valueOf(distance));
+                this.ctw.getShooterAchievementHandler().checkForAchievements(killer);
+                this.ctw.getOverpoweredAchievementHandler().checkForAchievements(killer);
+                this.ctw.getDistanceAchievementHandler().checkForAchievements(killer);
+                this.ctw.getKillStreakHandler().addStreakKill(killer);
+                this.ctw.getPlayerKillsHandler().addBowKillMatch(killer);
+                
+                addPoints2(p, killer, bowDeathBroadcast);
+                addRegen(killer);
             } else if (p.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.VOID || p.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.LAVA || p.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.FALL) {
-                final Player killer = Death.this.ctw.getLastDamageHandler().getKiller(p);
-                if (killer != null) {
-                    addRegen(killer);
-                    String icon = Death.this.ctw.getLanguageHandler().getMessage("Icons.Other");
-                    String rawMsg4 = Death.this.ctw.getLanguageHandler().getMessage("ChatMessages.VoidDeathBroadcast").replace("%WeaponLogo%", icon);
-                    addPoints(p, killer, rawMsg4);
-                    Death.this.ctw.getKillStreakHandler().addStreakKill(killer);
-                    final String weaponType = Death.this.ctw.getLastDamageHandler().getWeaponType(p);
+                Player lastDamagerKiller = this.ctw.getLastDamageHandler().getKiller(p);
+                if (lastDamagerKiller != null) {
+                    addRegen(lastDamagerKiller);
+                    String icon = this.ctw.getLanguageHandler().getMessage("Icons.Other");
+                    String rawMsg4 = this.ctw.getLanguageHandler().getMessage("ChatMessages.VoidDeathBroadcast").replace("%WeaponLogo%", icon);
+                    addPoints(p, lastDamagerKiller, rawMsg4);
+                    this.ctw.getKillStreakHandler().addStreakKill(lastDamagerKiller);
+                    final String weaponType = this.ctw.getLastDamageHandler().getWeaponType(p);
                     if (weaponType.matches("melee")) {
-                        Death.this.ctw.getPlayerKillsHandler().addMeleeKill(killer);
-                        Death.this.ctw.getMeleeAchievementHandler().checkForAchievements(killer);
-                        Death.this.ctw.getOverpoweredAchievementHandler().checkForAchievements(p);
+                        this.ctw.getPlayerKillsHandler().addMeleeKill(lastDamagerKiller);
+                        this.ctw.getMeleeAchievementHandler().checkForAchievements(lastDamagerKiller);
+                        this.ctw.getOverpoweredAchievementHandler().checkForAchievements(p);
 
                     } else if (weaponType.matches("bow")) {
-                        Death.this.ctw.getPlayerKillsHandler().addBowKill(killer);
-                        Death.this.ctw.getShooterAchievementHandler().checkForAchievements(killer);
-                        Death.this.ctw.getOverpoweredAchievementHandler().checkForAchievements(killer);
-                        Death.this.ctw.getDistanceAchievementHandler().checkForAchievements(killer);
+                        this.ctw.getPlayerKillsHandler().addBowKill(lastDamagerKiller);
+                        this.ctw.getShooterAchievementHandler().checkForAchievements(lastDamagerKiller);
+                        this.ctw.getOverpoweredAchievementHandler().checkForAchievements(lastDamagerKiller);
+                        this.ctw.getDistanceAchievementHandler().checkForAchievements(lastDamagerKiller);
                     }
-                } else if (!Death.this.ctw.getTeamHandler().isSpectator(p)) {
-
+                } else if (!this.ctw.getTeamHandler().isSpectator(p)) {
 
                     if (this.ctw.getGameEngine().gameStage != GameEngine.GameStages.COUNTDOWN) {
                         if ((ctw.getTeamHandler().countBlueTeam() + ctw.getTeamHandler().countRedTeam()) > 3) {
 
-                            Death.this.ctw.getPlayerScoreHandler().takeScore(p, Death.this.ctw.getConfigHandler().getInteger("Rewards.Score.death"));
-                            Death.this.ctw.getMessageUtils().sendScoreMessage(p, "-" + Death.this.ctw.getConfigHandler().getInteger("Rewards.Score.death"), null);
+                            this.ctw.getPlayerScoreHandler().takeScore(p, this.ctw.getConfigHandler().getInteger("Rewards.Score.death"));
+                            this.ctw.getMessageUtils().sendScoreMessage(p, "-" + this.ctw.getConfigHandler().getInteger("Rewards.Score.death"), null);
                         }
                     }
                 }
             }
-
-            new BukkitRunnable() {
-                public void run() {
-                    p.spigot().respawn();
-                }
-            }.runTaskLater(this.ctw, 1L);
         });
 
+        Bukkit.getScheduler().runTask(this.ctw, () -> player.spigot().respawn());
     }
 
     private void addPoints(Player p, Player killer, @NotNull String rawMsg4) {
-
-        final String rawMsg5 = rawMsg4.replace("%KilledPlayer%", Death.this.ctw.getMessageUtils().getTeamColor(p));
-        final String rawMsg6 = rawMsg5.replace("%Killer%", Death.this.ctw.getMessageUtils().getTeamColor(killer));
+        final String rawMsg5 = rawMsg4.replace("%KilledPlayer%", this.ctw.getMessageUtils().getTeamColor(p));
+        final String rawMsg6 = rawMsg5.replace("%Killer%", this.ctw.getMessageUtils().getTeamColor(killer));
         addPoints2(p, killer, rawMsg6);
-
     }
 
     private void addPoints2(Player p, Player killer, @NotNull String rawMsg6) {
 
-        Death.this.sendDeathMessage(rawMsg6.replaceAll("&", "§"));
-        if (this.ctw.getGameEngine().gameStage != GameEngine.GameStages.COUNTDOWN) {
+        this.sendDeathMessage(rawMsg6.replaceAll("&", "§"));
+        if (this.ctw.getGameEngine().gameStage == GameEngine.GameStages.RUNNING) {
 
-            ctw.getPlayerKillsHandler().addKillMatch(killer);
+            if ((ctw.getTeamHandler().countBlueTeam() + ctw.getTeamHandler().countRedTeam()) > 3 || killer.isOp()) {
 
-            if ((ctw.getTeamHandler().countBlueTeam() + ctw.getTeamHandler().countRedTeam()) > 3) {
+                // Quitar puntos al jugador que muere
+                this.ctw.getPlayerScoreHandler().takeScore(p, this.ctw.getConfigHandler().getInteger("Rewards.Score.death"));
+                this.ctw.getMessageUtils().sendScoreMessage(p, "-" + this.ctw.getConfigHandler().getInteger("Rewards.Score.death"), null);
 
-                Death.this.ctw.getPlayerScoreHandler().takeScore(p, Death.this.ctw.getConfigHandler().getInteger("Rewards.Score.death"));
-                Death.this.ctw.getMessageUtils().sendScoreMessage(p, "-" + Death.this.ctw.getConfigHandler().getInteger("Rewards.Score.death"), null);
-                Death.this.ctw.getPlayerScoreHandler().addScore(killer, Death.this.ctw.getConfigHandler().getInteger("Rewards.Score.kill"));
-                Death.this.ctw.getEconomyHandler().addCoins(killer, (double) Death.this.ctw.getConfigHandler().getInteger("Rewards.Coins.kill"));
-                Death.this.ctw.getMessageUtils().sendScoreMessage(killer, "+" + Death.this.ctw.getConfigHandler().getInteger("Rewards.Score.kill"), Death.this.ctw.getConfigHandler().getInteger("Rewards.Coins.kill"));
+                // Añadir puntos al jugador que mata
+                this.ctw.getPlayerScoreHandler().addScore(killer, this.ctw.getConfigHandler().getInteger("Rewards.Score.kill"));
+                this.ctw.getEconomyHandler().addCoins(killer, Double.valueOf(this.ctw.getConfigHandler().getInteger("Rewards.Coins.kill")));
+                this.ctw.getMessageUtils().sendScoreMessage(killer, "+" + this.ctw.getConfigHandler().getInteger("Rewards.Score.kill"), this.ctw.getConfigHandler().getInteger("Rewards.Coins.kill"));
             }
         }
     }
@@ -240,27 +232,23 @@ public class Death implements Listener {
 
         if (this.ctw.getGameEngine().gameStage != GameEngine.GameStages.COUNTDOWN) {
             killer.getInventory().addItem(new ItemStack(Material.GOLDEN_APPLE));
-            killer.getInventory().addItem(new ItemStack(Material.ARROW, 12));
+            killer.getInventory().addItem(new ItemStack(Material.ARROW, 2));
         }
 
         if (!checkArmor(killer)) return;
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
+        Bukkit.getScheduler().runTaskLater(this.ctw, () -> {
+            if (this.ctw.getKillStreakHandler().getStreak(killer) <= 3) {
+                killer.removePotionEffect(PotionEffectType.REGENERATION);
+                killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 80, 1));
+            } else {
+                killer.removePotionEffect(PotionEffectType.ABSORPTION);
+                killer.removePotionEffect(PotionEffectType.REGENERATION);
 
-                if (Death.this.ctw.getKillStreakHandler().getStreak(killer) <= 3) {
-                    killer.removePotionEffect(PotionEffectType.REGENERATION);
-                    killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 80, 1));
-                } else {
-                    killer.removePotionEffect(PotionEffectType.ABSORPTION);
-                    killer.removePotionEffect(PotionEffectType.REGENERATION);
-
-                    killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 1));
-                    killer.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 200, 0));
-                }
+                killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 1));
+                killer.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 200, 0));
             }
-        }.runTaskLater(ctw, 3);
+        }, 3L);
     }
 
     private String getWeaponLogo(final Player p) {
@@ -283,7 +271,6 @@ public class Death implements Listener {
             }
         } catch (Exception e) {
             CTW.log.warning("Error getting weapon logo for the weapon used! Error: " + e.getMessage());
-            e.printStackTrace();
         }
         return icon;
     }
