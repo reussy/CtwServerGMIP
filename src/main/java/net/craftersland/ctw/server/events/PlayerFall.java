27c342f68eba3f018e858d1fcd257049e0439fed
Ricardo Araque <ricardoaraque@outlook.com>
2025-02-15 11:06:09 -0600

    Agregado comentario

--- src/main/java/net/craftersland/ctw/server/events/PlayerFall.java
+++ src/main/java/net/craftersland/ctw/server/events/PlayerFall.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class PlayerFall implements Listener {
     private final CTW ctw;
@@ -14,12 +15,12 @@ public class PlayerFall implements Listener {
         this.ctw = ctw;
     }
 
+    /*
+        * Cancel fall damage if the player falls less than 7 blocks
+     */
     @EventHandler
-    public void onPlayerFall(EntityDamageEvent e) {
-
-        if (e.getEntity() instanceof Player) {
-            Player player = (Player) e.getEntity();
-
+    public void onPlayerFall(@NotNull EntityDamageEvent e) {
+        if (e.getEntity() instanceof Player player) {
             if (e.getCause() == EntityDamageEvent.DamageCause.FALL) {
                 if (player.getFallDistance() <= 7) {
                     e.setCancelled(true);
