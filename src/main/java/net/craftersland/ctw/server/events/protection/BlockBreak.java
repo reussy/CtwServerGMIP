8b40fa424d0227bb4209ee85b20c5b8f3d3cce5f
Ricardo Araque <ricardoaraque@outlook.com>
2025-03-05 20:45:01 -0600

    Optimizado block break

--- src/main/java/net/craftersland/ctw/server/events/protection/BlockBreak.java
+++ src/main/java/net/craftersland/ctw/server/events/protection/BlockBreak.java
@@ -6,15 +6,23 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BlockBreak implements Listener {
     private final CTW ctw;
+    private final List<Location> woolLocations;
 
     public BlockBreak(final CTW ctw) {
         this.ctw = ctw;
+        this.woolLocations = new ArrayList<>();
+        addWoolLocation();
     }
 
     @EventHandler
@@ -23,88 +31,57 @@ public class BlockBreak implements Listener {
             event.setCancelled(true);
             return;
         }
-        final Location l = event.getBlock().getLocation();
-        if (l.equals(this.ctw.getMapConfigHandler().redWool)) {
-            if (event.getPlayer() != null) {
-                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-                    event.setCancelled(true);
-                }
-            } else {
+
+        Player player = event.getPlayer();
+
+        if (player == null) return;
+
+        if (player.getGameMode() == GameMode.CREATIVE) {
+            event.setCancelled(true);
+            return;
+        }
+
+        final Location location = event.getBlock().getLocation();
+
+        // Chequeamos si la ubicación del bloque roto es una lana
+        woolLocations.forEach(woolLocation -> {
+            if (location.equals(woolLocation)) {
+                event.setCancelled(true);
+            }
+        });
+
+       // Protegemos las áreas si están protegidas
+        if (this.ctw.getProtectionHandler().isAreaProtected(location)) {
+            if (event.getBlock().getType() != Material.IRON_BLOCK
+                    || event.getBlock().getType() != Material.GOLD_BLOCK
+                    || event.getBlock().getType() != Material.WEB) {
                 event.setCancelled(true);
             }
             return;
         }
-        if (l.equals(this.ctw.getMapConfigHandler().pinkWool)) {
-            if (event.getPlayer() != null) {
-                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-                    event.setCancelled(true);
-                }
-            } else {
-                event.setCancelled(true);
-            }
-            return;
-        }
-        if (l.equals(this.ctw.getMapConfigHandler().blueWool)) {
-            if (event.getPlayer() != null) {
-                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-                    event.setCancelled(true);
-                }
-            } else {
-                event.setCancelled(true);
-            }
-            return;
-        }
-        if (l.equals(this.ctw.getMapConfigHandler().cyanWool)) {
-            if (event.getPlayer() != null) {
-                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-                    event.setCancelled(true);
-                }
-            } else {
-                event.setCancelled(true);
-            }
-            return;
-        }
-        if (this.ctw.getProtectionHandler().isAreaProtected(l)) {
-            if (event.getPlayer() != null) {
-                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-                    if (event.getBlock().getType() != Material.IRON_BLOCK && event.getBlock().getType() != Material.GOLD_BLOCK || event.getBlock().getType() != Material.WEB) {
-                        event.setCancelled(true);
-                    }
-                }
-            } else {
-                event.setCancelled(true);
-            }
-            return;
-        }
+
         this.removeDrops(event);
     }
 
-    private void removeDrops(final BlockBreakEvent event) {
-        final Block b = event.getBlock();
-        if (b.getType() == Material.WEB) {
+    private void removeDrops(final @NotNull BlockBreakEvent event) {
+        final Block block = event.getBlock();
+        if (block.getType() == Material.WEB ||
+                block.getType() == Material.REDSTONE_WIRE ||
+                block.getType() == Material.LEAVES ||
+                block.getType() == Material.LEAVES_2 ||
+                block.getType() == Material.YELLOW_FLOWER ||
+                block.getType() == Material.RED_ROSE ||
+                block.getType() == Material.WOOL ||
+                block.getType() == Material.ICE) {
             event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.REDSTONE_WIRE) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.LEAVES) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.LEAVES_2) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.YELLOW_FLOWER) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.RED_ROSE) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.WOOL) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
-        } else if (b.getType() == Material.ICE) {
-            event.setCancelled(true);
-            b.setType(Material.AIR);
+            block.setType(Material.AIR);
         }
+    }
+
+    private void addWoolLocation() {
+        this.woolLocations.add(this.ctw.getMapConfigHandler().blueWool);
+        this.woolLocations.add(this.ctw.getMapConfigHandler().cyanWool);
+        this.woolLocations.add(this.ctw.getMapConfigHandler().pinkWool);
+        this.woolLocations.add(this.ctw.getMapConfigHandler().redWool);
     }
 }
